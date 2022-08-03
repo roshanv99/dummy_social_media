61e796cb3d491c3c46cb10a28ac52028ec9130af
Roshan Varghese <roshan.varghese612@gmail.com>
2022-08-03 19:04:22 +0530

    03 August

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,5 @@
 const Post = require('../../models/Post')
+const checkAuth = require('../../util/check-auth')
 
 module.exports = {
     Query:{
@@ -10,7 +11,36 @@ module.exports = {
             catch(e){
                 throw new Error(e)
             }
+        },
 
+        async getPost(_, { postId }){
+            try{
+                const post = await Post.findById(postId);
+                if(post){
+                    return post;
+                } else{
+                    throw new Error("Post not found");
+                }
+            }
+            catch(err){
+                throw new Error(err)
+            }
+        },
+        
+    },
+
+    Mutation: {
+        async createPost(_, { body }, context){
+            const user = checkAuth(context);
+            const newPost = new Post({
+                body,
+                user : user.id,
+                username:user.username,
+                createdAt: new Date().toISOString
+            });
+
+            const post = await newPost.save();
+            return post;
         }
     }
 }
--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -24,11 +24,14 @@ module.exports = gql`
     }
 
     type Query{
-        getPosts : [Post] 
+        getPosts : [Post]
+        getPost(postId:ID!):Post
     } 
 
     type Mutation{
         register(registerInput:RegisterInput) : User!
         login(username:String!, password:String!): User! #Returns user
+        createPost(body: String!): Post!
+        deletePost(postId: ID!): String!
     }
 `
