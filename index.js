96600ba53c6db33abcf9325c19283f50d1a097fb
Roshan Varghese <roshan.varghese612@gmail.com>
2022-08-20 23:32:00 +0530

    Last

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { MONGODB } = require('./config.js')
 const resolvers = require('./graphql/resolvers');
 const typeDefs = require('./graphql/typeDefs') 
 
-
+const PORT = process.env.port || 5000;
 // const pubsub = new PubSub();
 //A PubSub instance enables your server code to both publish events to a particular label and listen for events associated with a particular label.
 const server = new ApolloServer({
@@ -19,8 +19,11 @@ const server = new ApolloServer({
 mongoose.connect(MONGODB, { useNewUrlParser: true })
     .then( () => {
         console.log(`MongoDB connected`);
-        return server.listen({ port:5000 });
+        return server.listen({ port:PORT });
     })
     .then(res => {
         console.log(`Server running at ${res.url}`);
+    })
+    .catch(err => {
+        console.log(err);
     })
